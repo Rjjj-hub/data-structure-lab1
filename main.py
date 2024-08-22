3eab42a450bc3d0c1a6c1ea3880aafadac6a282f
Ruby Jan N. Arevalo <oyoboooyarevalo@gmail.com>
2024-08-22 11:47:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# FNumber = input("Enter First Number: ")
+# SNumber = input("Enter Second Number: ")
+# if FNumber < SNumber:
+#     print(FNumber + " is Lower than " + SNumber)
+# elif FNumber > SNumber:
+#     print(FNumber + " is Greater than " + SNumber)
+# else:
+#     print(FNumber + " is equal to " + SNumber )
+
+n1 = input("Enter classmate name 1: ")
+n2 = input("Enter classmate name 2: ")
+n3 = input("Enter classmate name 3: ")
+
+print("Your classmate are: \n" + n1 + " \n" + n2 + " \n" + n3)
